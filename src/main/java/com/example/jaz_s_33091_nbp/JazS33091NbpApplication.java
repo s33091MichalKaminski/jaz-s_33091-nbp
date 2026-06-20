eb178fc78ba71ccd88cfc7a5d8e17a285adf7f45
s33091MichalKaminski <michal.kaminski232@gmail.com>
2026-06-20 12:22:44 +0200

    Add files via upload

---
+++ src/main/java/com/example/jaz_s_33091_nbp/JazS33091NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s_33091_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS33091NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS33091NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaz_s_33091_nbp/JazS33091NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s_33091_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS33091NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
